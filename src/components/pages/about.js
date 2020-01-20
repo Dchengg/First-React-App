94923ef6df9bdf9d35c001037bcb031e5bf16281
Dchengg <31906856+Dchengg@users.noreply.github.com>
2020-01-19 21:57:09 -0600

    Todos website

---
+++ src/components/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function about() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is my first react app, let's hope I can do a lot more from now on</p>
+        </React.Fragment>
+    )
+}
+
+export default about;
